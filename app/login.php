6c3f5b5e9bbf88a745f67e30ee4b1a59b17d998a
iammutevu <ianmutevu96@gmail.com>
2022-05-24 15:19:08 +0300

    Update folder/file naming and structure

--- app/login.php
+++ app/login.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/Scripts/login.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
 
     <title>LOGIN FORM</title>
   </head>
