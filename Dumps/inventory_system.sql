c453e1cb4a3cdff09095a65722b77d3bb31b59b0
Kisha-net <antoinette.akinyi@azubiafrica.org>
2022-07-06 10:11:29 +0300

    updating the dumps folder

--- Dumps/inventory_system.sql
+++ Dumps/inventory_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2022 at 01:38 PM
+-- Generation Time: Jul 06, 2022 at 09:02 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -24,22 +24,85 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `item`
+-- Table structure for table `customers`
 --
 
-CREATE TABLE `item` (
-  `itemid` int(11) NOT NULL,
-  `itemname` varchar(100) NOT NULL,
-  `productid` int(11) DEFAULT NULL,
-  `itemstatus` varchar(100) DEFAULT NULL
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `names` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `item`
+-- Dumping data for table `customers`
 --
 
-INSERT INTO `item` (`itemid`, `itemname`, `productid`, `itemstatus`) VALUES
-(1, '', NULL, NULL);
+INSERT INTO `customers` (`customer_id`, `names`, `email`) VALUES
+(1, 'Antoinette Akinyi', 'antoinette@gmail.com'),
+(3, 'kysher', 'kysher@gmail.com'),
+(5, 'person y', 'y@gmail.com'),
+(6, 'person XY', 'XY@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `item_id` int(11) NOT NULL,
+  `item_name` varchar(255) DEFAULT NULL,
+  `stock` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_name`, `stock`, `price`) VALUES
+(21, 'keys', 'Available', 400),
+(23, 'bottle', 'Available', 1000),
+(24, 'phone', 'Available', 28000),
+(27, 'books', 'Available', 350);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_name` varchar(100) DEFAULT NULL,
+  `order_status` varchar(255) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `customer_name` varchar(255) DEFAULT NULL,
+  `order_date` varchar(255) DEFAULT NULL,
+  `delivery_date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_name`, `order_status`, `total`, `customer_name`, `order_date`, `delivery_date`) VALUES
+(1038, 'bottle', NULL, 0, 'person y', '05/07/2022', '2022-07-21'),
+(1039, 'phone', NULL, 0, 'person XY', '05/07/2022', '2022-07-21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -51,47 +114,89 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `username` varchar(128) NOT NULL,
   `useremail` varchar(128) NOT NULL,
-  `userpwd` varchar(128) NOT NULL
+  `userpwd` varchar(128) NOT NULL,
+  `passwordRepeat` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `useremail`, `userpwd`) VALUES
-(10, 'Kisha_net', 'akinyiantoinette6@gmail.com', '$2y$10$0S9e8naaE8zcvlKj4cODSu2.x3bNeyyXhenU3mpcNYLoH0ytyPD2K');
+INSERT INTO `users` (`user_id`, `username`, `useremail`, `userpwd`, `passwordRepeat`) VALUES
+(40, 'dummy', 'dummy@gmail.com', '$2y$10$9zAK3iN.guw0fgutZG0HDOBkLUdpF9suoSH6S6eGosyfvH63vRIzG', '$2y$10$Tt7n5YAi3MtyZhWuOxAjfOqtFz4LNJJXg7ABsQdqTZzO0ufKuRHju');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `item`
+-- Indexes for table `customers`
 --
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`itemid`);
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `useremail` (`useremail`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `item`
+-- AUTO_INCREMENT for table `customers`
 --
-ALTER TABLE `item`
-  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1040;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
